51f7a3e60b3575b7727ad58fa9e925a632229a50
ryan-cr <ryanriley@csus.edu>
2018-03-09 14:10:47 -0800

    Preemptively configured project data Configured maven modules (desktop-client, server-api) Created root lombok.config to stop bubbling Created placeholder entry-point main classes

---
+++ desktop-client/src/main/java/edu/csus/yaam/client/YaamClientLauncher.java
@@ -0,0 +1,14 @@
+package edu.csus.yaam.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Ryan R
+ * @date 3/8/2018
+ */
+public class YaamClientLauncher extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    }
+}
