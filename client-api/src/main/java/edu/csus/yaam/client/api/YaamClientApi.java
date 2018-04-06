133f13837f1ea11fddf2fff07f3307fedbf447fa
paul-mchugh <paulpjmmchugh@gmail.com>
2018-04-05 17:00:33 -0700

    renamed YaamWebclient to YaamClientApi

---
+++ client-api/src/main/java/edu/csus/yaam/client/api/YaamClientApi.java
@@ -0,0 +1,13 @@
+package edu.csus.yaam.client.api;
+
+/**
+ * @author Paul M
+ * @date 4/5/2018
+ */
+public class YaamClientApi
+{
+	public YaamClientApi()
+	{
+	
+	}
+}
--- client-api/src/main/java/edu/csus/yaam/client/api/YaamWebclient.java
+++
@@ -1,8 +0,0 @@
-package edu.csus.yaam.client.api;
-
-/**
- * @author Ryan R
- * @date 3/30/2018
- */
-public class YaamWebclient {
-}
