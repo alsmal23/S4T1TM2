aec8717384ce3a48f259a0abb3383c2b63241174
paul-mchugh <paulpjmmchugh@gmail.com>
2018-04-08 22:46:02 -0700

    Created callback interface

---
+++ client-api/src/main/java/edu/csus/yaam/client/api/ClientAPICallback.java
@@ -0,0 +1,10 @@
+package edu.csus.yaam.client.api;
+
+/**
+ * Created by paulp on 4/8/2018.
+ */
+interface ClientAPICallback
+{
+	void didConnectToServerSucessfully();
+	void failedToConnectToServer(String reason);
+}
--- client-api/src/main/java/edu/csus/yaam/client/api/YaamClientApi.java
+++ client-api/src/main/java/edu/csus/yaam/client/api/YaamClientApi.java
@@ -1,13 +1,28 @@
 package edu.csus.yaam.client.api;
 
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author Paul M
  * @date 4/5/2018
  */
 public class YaamClientApi
 {
-	public YaamClientApi()
-	{
+	protected ClientAPICallback callback;
+	protected URL remoteHost;
 	
+	/*
+	The URL parameter host is used only for the host name.
+	Callback must not be null.
+	 */
+	public YaamClientApi(String host,  ClientAPICallback clientCallback) throws MalformedURLException
+	{
+		//make sure that the callback if not null
+		if(callback==null) throw new NullPointerException("Client-API callback must not be null");
+		this.callback = callback;
+		
+		remoteHost = new URL("http", host,80 ,"");
 	}
 }
