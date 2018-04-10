777f05609a4da5a37753d50b47bfdf0a9e1ef60d
Lindsay <mightymouse@gmail.com>
2018-04-10 14:20:51 -0700

    Updates login screen demo. All elements are present, but not pretty or interactable.

--- desktop-client/src/main/java/edu/csus/yaam/client/demo/LoginDemo.java
+++ desktop-client/src/main/java/edu/csus/yaam/client/demo/LoginDemo.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.geometry.BoundingBox;
 import com.sun.javafx.binding.StringConstant;
 import javafx.event.EventHandler;
 import javafx.scene.control.ChoiceBox;
@@ -39,16 +40,38 @@ public class LoginDemo extends Application {
         Pane rootPane = new Pane();
 
 
-        // ----------- USER NAME TEXT BOX -------------
-        JFXTextField field = new JFXTextField();
-        field.setPromptText("Placeholder value...");
-        primaryStage.titleProperty().bind(field.textProperty());
-        field.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(field.widthProperty().divide(2)));
-        field.layoutYProperty().bind(rootPane.heightProperty().multiply(.5).subtract(field.heightProperty().divide(2)));
-        field.setFocusColor(Color.GREEN);
-        field.setUnFocusColor(Color.RED);
-        field.setStyle("-fx-background-color: light-gray;");
-        rootPane.getChildren().add(field);
+        //------------ BOUNDING BOX ------------ Focuses the user onto important elements of the page
+        //BoundingBox loginBox = new BoundingBox(50, 50, 100, 100); //double minX, double minY, double width, double height
+        //rootPane.getChildren().add(loginBox);
+
+        // ----------- INPUTS AND BUTTONS -------------
+        JFXTextField username = new JFXTextField();
+        username.setPromptText("Placeholder value...");
+        username.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(username.widthProperty().divide(2)));
+        username.layoutYProperty().bind(rootPane.heightProperty().multiply(.3).subtract(username.heightProperty().divide(2)));
+        username.setFocusColor(Color.GREEN);
+        username.setUnFocusColor(Color.RED);
+        rootPane.getChildren().add(username);
+
+        JFXTextField password = new JFXTextField();
+        password.setPromptText("Placeholder value...");
+        password.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(password.widthProperty().divide(2)));
+        password.layoutYProperty().bind(rootPane.heightProperty().multiply(.4).subtract(password.heightProperty().divide(2)));
+        password.setFocusColor(Color.GREEN);
+        password.setUnFocusColor(Color.RED);
+        password.setStyle("-fx-text-box-border: LightGray;");
+        rootPane.getChildren().add(password);
+
+        JFXButton loginButton = new JFXButton("Login");
+        loginButton.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(password.widthProperty().divide(2)));
+        loginButton.layoutYProperty().bind(rootPane.heightProperty().multiply(.5).subtract(password.heightProperty().divide(2)));
+        rootPane.getChildren().add(loginButton);
+
+        JFXButton newAccountButton = new JFXButton("Create a New Account");
+        newAccountButton.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(password.widthProperty().divide(2)));
+        newAccountButton.layoutYProperty().bind(rootPane.heightProperty().multiply(.6).subtract(password.heightProperty().divide(2)));
+        rootPane.getChildren().add(newAccountButton);
+
 
 
         // ----------- DEFINE/SET STAGE/SCENE -------------
