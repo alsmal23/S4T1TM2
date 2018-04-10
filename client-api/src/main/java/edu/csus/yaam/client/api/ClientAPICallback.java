c9f04fdd363b1a2a59607152768a99ad264f45f7
paul-mchugh <paulpjmmchugh@gmail.com>
2018-04-10 14:06:12 -0700

    Changed ClientAPICallback to combine methods

--- client-api/src/main/java/edu/csus/yaam/client/api/ClientAPICallback.java
+++ client-api/src/main/java/edu/csus/yaam/client/api/ClientAPICallback.java
@@ -3,13 +3,11 @@ package edu.csus.yaam.client.api;
 /**
  * Created by paulp on 4/8/2018.
  */
-interface ClientAPICallback
+public interface ClientAPICallback
 {
-	void didConnectToServerSucessfully();
-	void failedToConnectToServer(String reason);
+	void serverStatusChange(ServerConnectionEvent event, String failureReason);//If the connection is lost then an error description will be passed in failureReason otherwise it will be null
 	void didAuthenticateSucessfully();
 	void failedToAuthenticate(String reason);
-	void serverConnectionLost();
 	void knownUsersSucessfullyRetrieved(User[] knownUsers);//is called with a list of all the Users that have projects in common with you
 	void projectsSucessfullyRetrieved(Project[] projects); //is called with a list of all the projects you are a member of
 }
---
+++ client-api/src/main/java/edu/csus/yaam/client/api/ServerConnectionEvent.java
@@ -0,0 +1,11 @@
+package edu.csus.yaam.client.api;
+
+/**
+ * Created by paulp on 4/10/2018.
+ */
+public enum ServerConnectionEvent
+{
+	CONNECTION_ESTABLISHED,
+	CONECTION_ATTEMPT_FAILED,
+	CONNECTION_LOST
+}
