852ae5b75f0f81ccae186f12fc96597a2981913b
Lindsay <mightymouse@gmail.com>
2018-04-10 22:16:50 -0700

    Adds better positioning code that supports resizing.

--- desktop-client/src/main/java/edu/csus/yaam/client/demo/LoginDemo.java
+++ desktop-client/src/main/java/edu/csus/yaam/client/demo/LoginDemo.java
@@ -45,21 +45,22 @@ public class LoginDemo extends Application {
 
 
         //------------ BOUNDING BOX ------------ Focuses the user onto important elements of the page
-        Rectangle r = new Rectangle();
-        r.setX(280);
-        r.setY(120);
-        r.setWidth(250);
-        r.setHeight(300);
-        r.setFill(Color.SNOW);
-        r.setStroke(Color.GRAY);
-        rootPane.getChildren().add(r);
+        Rectangle loginBoundingBox = new Rectangle();
+        loginBoundingBox.setWidth(250);
+        loginBoundingBox.setHeight(300);
+        loginBoundingBox.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(loginBoundingBox.widthProperty().divide(2)));
+        loginBoundingBox.layoutYProperty().bind(rootPane.heightProperty().multiply(.5f).subtract(loginBoundingBox.heightProperty().divide(2)));
+
+        loginBoundingBox.setFill(Color.SNOW);
+        loginBoundingBox.setStroke(Color.GRAY);
+        rootPane.getChildren().add(loginBoundingBox);
 
 
         // ----------- INPUTS AND BUTTONS -------------
         JFXTextField usernameField = new JFXTextField();
         usernameField.setPromptText("Username");
         usernameField.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(usernameField.widthProperty().divide(2)));
-        usernameField.layoutYProperty().bind(rootPane.heightProperty().multiply(.3).subtract(usernameField.heightProperty().divide(2)));
+        usernameField.layoutYProperty().bind(rootPane.heightProperty().multiply(.5f).subtract(loginBoundingBox.heightProperty().divide(2).subtract(30)));
         usernameField.setFocusColor(Color.GREEN);
         rootPane.getChildren().add(usernameField);
 
@@ -69,21 +70,21 @@ public class LoginDemo extends Application {
         passwordField.setFocusColor(Color.GREEN);
         passwordField.setStyle("-fx-text-box-border: Snow;");
         passwordField.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(passwordField.widthProperty().divide(2)));
-        passwordField.layoutYProperty().bind(rootPane.heightProperty().multiply(.4).subtract(passwordField.heightProperty().divide(2)));
+        passwordField.layoutYProperty().bind(rootPane.heightProperty().multiply(.5f).subtract(loginBoundingBox.heightProperty().divide(2).subtract(70)));
         rootPane.getChildren().add(passwordField);
 
 
         Text loginErrorText = new Text("Account not found");
         loginErrorText.setFill(Color.SNOW);
         loginErrorText.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(passwordField.widthProperty().divide(2)));
-        loginErrorText.layoutYProperty().bind(rootPane.heightProperty().multiply(.5).subtract(passwordField.heightProperty().divide(2)));
+        loginErrorText.layoutYProperty().bind(rootPane.heightProperty().multiply(.5f).subtract(loginBoundingBox.heightProperty().divide(2).subtract(120)));
         rootPane.getChildren().add(loginErrorText);
 
 
         JFXButton loginButton = new JFXButton("LOGIN"); //Can we make this bold?
         loginButton.setBackground(new Background(new BackgroundFill(Color.rgb(225, 225, 225), null, null)));
         loginButton.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(loginButton.widthProperty().divide(2)));
-        loginButton.layoutYProperty().bind(rootPane.heightProperty().multiply(.6).subtract(loginButton.heightProperty().divide(2)));
+        loginButton.layoutYProperty().bind(rootPane.heightProperty().multiply(.5f).subtract(loginBoundingBox.heightProperty().divide(2).subtract(140)));
         rootPane.getChildren().add(loginButton);
 
         loginButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -98,7 +99,7 @@ public class LoginDemo extends Application {
 
         JFXButton newAccountButton = new JFXButton("Create account");
         newAccountButton.layoutXProperty().bind(rootPane.widthProperty().multiply(.5f).subtract(newAccountButton.widthProperty().divide(2)));
-        newAccountButton.layoutYProperty().bind(rootPane.heightProperty().multiply(.7).subtract(newAccountButton.heightProperty().divide(2)));
+        newAccountButton.layoutYProperty().bind(rootPane.heightProperty().multiply(.5f).subtract(loginBoundingBox.heightProperty().divide(2).subtract(240)));
         rootPane.getChildren().add(newAccountButton);
 
 
